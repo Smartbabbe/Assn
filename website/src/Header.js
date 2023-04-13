77c7f26ea89587400759e6e5ebaa060d1472df8c
Smartbabbe <estherisrael036@gmail.com>
2023-04-13 19:50:01 +0100

    commit

--- website/src/Header.js
+++ website/src/Header.js
@@ -1,14 +1,19 @@
 const Intro = () => {
+
+    const Link = "https://react.org"
     return (
+
+       
+
     <div>
         <header>
             <h1>Welcome to my website!</h1>
             <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href={Link}>Home</a></li>
+                <li><a href={Link}>About</a></li>
+                <li><a href={Link}>Services</a></li>
+                <li><a href={Link}>Contact</a></li>
             </ul>
             </nav>
         </header>
